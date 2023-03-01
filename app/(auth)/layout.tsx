ae806fdc2667565ef2e893fac0acdc33859c7f1f
S4ntiego <adamksiazek123@gmail.com>
2023-03-01 13:38:20 +0100

    auth layout typo

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,12 +1,15 @@
 "use client"
 
-import React from "react"
-import { ToastContainer } from "react-toastify"
+import React, { FC } from "react"
 
 import "react-toastify/dist/ReactToastify.css"
 
+interface LayoutProps {
+  children: React.ReactNode
+}
+
 //AuthLayout component is a functional component that takes an object as its single argument, which should have the shape described by the AuthLayoutProps interface.
 //In this case, the AuthLayout component expects to receive props with the shape described by the AuthLayoutProps interface.
-export default function AuthLayout(children?: React.ReactNode) {
+export default function AuthLayout({ children }: LayoutProps) {
   return <div className="min-h-screen">{children}</div>
 }
