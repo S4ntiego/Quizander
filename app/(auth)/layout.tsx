bae58699afd08b408d0891cb63b7e23600bc4b4f
S4ntiego <adamksiazek123@gmail.com>
2023-03-01 13:28:04 +0100

    fixed auth layout type

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -7,11 +7,6 @@ import "react-toastify/dist/ReactToastify.css"
 
 //AuthLayout component is a functional component that takes an object as its single argument, which should have the shape described by the AuthLayoutProps interface.
 //In this case, the AuthLayout component expects to receive props with the shape described by the AuthLayoutProps interface.
-export default function AuthLayout(children?: React.ReactNode) {
-  return (
-    <div className="min-h-screen">
-      <ToastContainer />
-      {children}
-    </div>
-  )
+export default function AuthLayout(children?: React.ReactElement) {
+  return <div className="min-h-screen">{children}</div>
 }
