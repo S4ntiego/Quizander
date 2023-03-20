afd4862d6aff785ade9d0850ebabd18c396df87f
S4ntiego <adamksiazek123@gmail.com>
2023-03-20 21:41:30 +0100

    fixed mobile landing, swapped to grid

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -9,35 +9,29 @@ import { Button, buttonVariants } from "./ui/button"
 
 const Landing = () => {
   return (
-    <section className="container flex flex-col flex-1 py-8 text-center items-center justify-center max-h-full">
-      <p className="font-lexend uppercase text-[13px] font-medium tracking-widest text-slate-800 dark:text-slate-300">
-        Yer a wizard, Harry.
-      </p>
-      <h1 className="font-playfair font-extrabold text-5xl">Harry Potter</h1>
-      <AspectRatio className="relative mt-8" ratio={2 / 3}>
+    <section className="flex-1 h-full grid grid-rows-6">
+      <div className="row-span-1 flex flex-col items-center justify-end">
+        <p className="uppercase tracking-widest text-[13px] font-lexend text-slate-300">
+          Yer a wizard, Harry
+        </p>
+        <h1 className="font-playfair text-5xl font-extrabold">Harry Potter</h1>
+      </div>
+      <div className="row-span-4 relative m-8 rounded-3xl">
         <Image
-          style={{ clipPath: "url(#hero-clip)" }}
-          fill
           src="https://d16toh0t29dtt4.cloudfront.net/hogwarts_train.png"
-          alt="hogwarts_train"
+          alt="hogwarts train"
+          fill
           className="object-cover rounded-3xl"
         />
-      </AspectRatio>
-      <div className="flex items-center justify-center">
-        <div className="flex flex-col py-8">
-          <a
-            href="#harry_potter_landing"
-            className={cn(
-              buttonVariants({ variant: "outline" }),
-              "rounded-3xl"
-            )}
-          >
-            <Icons.arrowDown className="w-3 h-3 rounded-full dark:text-slate-50 text-slate-900" />
-            <p className="ml-2 text-[13px] dark:text-slate-300">
-              Harry Potter Quizzes
-            </p>
-          </a>
-        </div>
+      </div>
+      <div className="row-span-1 flex items-start justify-center">
+        <a
+          href="#harry_potter_landing"
+          className={cn(buttonVariants({ variant: "outline" }))}
+        >
+          <Icons.arrowDown className="w-3 h-3 mr-2" />
+          <p>Harry Potter Quizzess</p>
+        </a>
       </div>
     </section>
   )
