015868a2884f4ff12b9027500d1158284e918673
S4ntiego <adamksiazek123@gmail.com>
2023-03-20 22:02:20 +0100

    adjusted mobile ui

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -9,29 +9,33 @@ import { Button, buttonVariants } from "./ui/button"
 
 const Landing = () => {
   return (
-    <section className="flex-1 h-full grid grid-rows-6">
-      <div className="row-span-1 flex flex-col items-center justify-end">
-        <p className="uppercase tracking-widest text-[13px] font-lexend text-slate-300">
+    <section className="flex-1 h-full grid grid-rows-5 container">
+      <div className="row-span-1 flex flex-col items-center text-center justify-end">
+        <p className="uppercase tracking-widest text-[13px] font-lexend dark:text-slate-300">
           Yer a wizard, Harry
         </p>
-        <h1 className="font-playfair text-5xl font-extrabold">Harry Potter</h1>
+        <h1 className="font-playfair text-5xl font-extrabold">
+          Harry Potter Quizzes
+        </h1>
       </div>
-      <div className="row-span-4 relative m-8 rounded-3xl">
-        <Image
-          src="https://d16toh0t29dtt4.cloudfront.net/hogwarts_train.png"
-          alt="hogwarts train"
-          fill
-          className="object-cover rounded-3xl"
-        />
-      </div>
-      <div className="row-span-1 flex items-start justify-center">
-        <a
-          href="#harry_potter_landing"
-          className={cn(buttonVariants({ variant: "outline" }))}
-        >
-          <Icons.arrowDown className="w-3 h-3 mr-2" />
-          <p>Harry Potter Quizzess</p>
-        </a>
+      <div className="grid grid-rows-6 row-span-4">
+        <div className="row-span-5 relative my-8 rounded-3xl">
+          <Image
+            src="https://d16toh0t29dtt4.cloudfront.net/hogwarts_train.png"
+            alt="hogwarts train"
+            fill
+            className="object-cover rounded-3xl"
+          />
+        </div>
+        <div className="row-span-1 flex items-start justify-center">
+          <a
+            href="#harry_potter_landing"
+            className={cn(buttonVariants({ variant: "outline" }))}
+          >
+            <Icons.arrowDown className="w-3 h-3 mr-2" />
+            <p>Verify your knowledge</p>
+          </a>
+        </div>
       </div>
     </section>
   )
