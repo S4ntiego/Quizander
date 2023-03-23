6b14fc23284b22ac23b9b145f362197787e71047
S4ntiego <adamksiazek123@gmail.com>
2023-03-23 09:18:30 +0100

    landing ui

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -11,12 +11,12 @@ const Landing = () => {
       <p className="uppercase text-[13px] font-bold text-dark-150">
         Yer a wizard, Harry
       </p>
-      <h1 className="font-bold font-lexend text-5xl tracking-tighter text-yellow-500">
+      <h1 className="font-bold leading-tight font-dancingscript text-6xl tracking-tighter text-yellow-500">
         Harry Potter Trivia
       </h1>
       <p className="mt-2 flex items-center justify-center px-4 text-dark-150"></p>
-      <div className="relative flex justify-center items-center h-96 w-screen">
-        <div className="absolute h-64 w-64 rounded-full bg-gradient-to-t from-yellow-500 via-yellow-900 to-dark-700 "></div>
+      <div className="relative flex justify-center items-center h-72 w-screen">
+        <div className="absolute h-64 w-64 rounded-full bg-gradient-to-t from-dark-500 to-dark-700 border dark:border-dark-500"></div>
         <Image
           src="https://i.ibb.co/sbmJfSQ/lecimytesty-magical-hat-from-harry-potter-render-plain-backgrou-88fc2326-8994-46ef-8c95-d6b3dfb1b3e3.png"
           alt="magic hat"
