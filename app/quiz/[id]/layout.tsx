832b711b65dda5303d303ff176646607c242676f
S4ntiego <adamksiazek123@gmail.com>
2023-04-02 15:28:58 +0200

    suspense refactor prod test

--- app/quiz/[id]/layout.tsx
+++ app/quiz/[id]/layout.tsx
@@ -1,10 +1,6 @@
-"use client"
-
-import React from "react"
+import React, { Suspense } from "react"
 
 import { dashboardConfig } from "@/config/dashboard"
-import Landing from "@/components/Landing"
-import { SiteFooter } from "@/components/SiteFooter"
 import { SiteHeader } from "@/components/SiteHeader"
 
 interface QuizzesProps {
@@ -18,7 +14,30 @@ const layout = ({ children }: QuizzesProps) => {
         mainConfig={dashboardConfig.mainNav}
         mobileConfig={dashboardConfig.mobileNav}
       />
-      <div className="flex flex-col h-[calc(100%-5rem)]">{children}</div>
+      <div className="flex flex-col h-[calc(100%-5rem)]">
+        <Suspense
+          fallback={
+            <div className="container h-full max-h-[100%-20rem] grid grid-rows-12 pb-8 xs:max-w-[54rem] overflow-auto">
+              <div className="grid grid-rows-[auto,1fr] row-span-4 justify-center text-center items-center xs:row-start-2">
+                <span className="text-xs xs:text-base flex justify-center">
+                  <div className="w-12 h-4 rounded-md animate-pulse bg-slate-500"></div>
+                </span>
+                <span className="m-auto text-base xs:text-2xl break-words overflow-auto">
+                  <div className="w-96 h-16 rounded-md animate-pulse bg-slate-500"></div>
+                </span>
+              </div>
+              <div className="grid grid-rows-4 row-span-8 gap-4 xs:mb-6 xs:row-span-6">
+                <div className="w-full rounded-md h-full animate-pulse bg-slate-500"></div>
+                <div className="w-full rounded-md h-full animate-pulse bg-slate-500"></div>
+                <div className="w-full rounded-md h-full animate-pulse bg-slate-500"></div>
+                <div className="w-full rounded-md h-full animate-pulse bg-slate-500"></div>
+              </div>
+            </div>
+          }
+        >
+          {children}
+        </Suspense>
+      </div>
     </div>
   )
 }
--- app/quiz/[id]/page.tsx
+++ app/quiz/[id]/page.tsx
@@ -4,22 +4,27 @@ import prisma from "@/lib/prisma"
 import { getCurrentUser } from "@/lib/session"
 import { Quiz } from "@/components/Quiz"
 
-async function getQuiz(quizId) {
-  await new Promise((resolve) => setTimeout(resolve, 3000))
-
-  const quiz = await prisma.quiz.findUnique({
-    where: { id: parseInt(quizId) },
-    include: {
-      questions: {
-        select: {
-          question: true,
-          answers: { select: { answer: true, isCorrect: true } },
+function getQuiz(quizId) {
+  return new Promise((resolve, reject) => {
+    prisma.quiz
+      .findUnique({
+        where: { id: parseInt(quizId) },
+        include: {
+          questions: {
+            select: {
+              question: true,
+              answers: { select: { answer: true, isCorrect: true } },
+            },
+          },
         },
-      },
-    },
+      })
+      .then((quiz) => {
+        resolve(JSON.parse(JSON.stringify(quiz)))
+      })
+      .catch((error) => {
+        reject(error)
+      })
   })
-
-  return JSON.parse(JSON.stringify(quiz))
 }
 
 interface QuizPageProps {
@@ -30,28 +35,5 @@ export default async function QuizPage({ params }: QuizPageProps) {
   const quiz = await getQuiz(params.id)
   const user = await getCurrentUser()
 
-  return (
-    <Suspense
-      fallback={
-        <div className="container h-full max-h-[100%-20rem] grid grid-rows-12 pb-8 xs:max-w-[54rem] overflow-auto">
-          <div className="grid grid-rows-[auto,1fr] row-span-4 justify-center text-center items-center xs:row-start-2">
-            <span className="text-xs xs:text-base flex justify-center">
-              <div className="w-12 h-4 rounded-md animate-pulse bg-slate-500"></div>
-            </span>
-            <span className="m-auto text-base xs:text-2xl break-words overflow-auto">
-              <div className="w-96 h-16 rounded-md animate-pulse bg-slate-500"></div>
-            </span>
-          </div>
-          <div className="grid grid-rows-4 row-span-8 gap-4 xs:mb-6 xs:row-span-6">
-            <div className="w-full rounded-md h-full animate-pulse bg-slate-500"></div>
-            <div className="w-full rounded-md h-full animate-pulse bg-slate-500"></div>
-            <div className="w-full rounded-md h-full animate-pulse bg-slate-500"></div>
-            <div className="w-full rounded-md h-full animate-pulse bg-slate-500"></div>
-          </div>
-        </div>
-      }
-    >
-      <Quiz quiz={quiz} user={user} />
-    </Suspense>
-  )
+  return <Quiz quiz={quiz} user={user} />
 }
