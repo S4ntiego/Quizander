b8268feb564a892d68c0c0b937fd642bfe8e63fe
S4ntiego <adamksiazek123@gmail.com>
2023-04-07 20:22:56 +0200

    auth test

--- app/(dashboard)/dashboard/scoreboard3/page.tsx
+++ app/(dashboard)/dashboard/scoreboard3/page.tsx
@@ -1,82 +1,27 @@
-import { redirect } from "next/navigation"
-import { User } from "@prisma/client"
+"use client"
+
+import React from "react"
 import { useSession } from "next-auth/react"
 
-import prisma from "@/lib/prisma"
 import { DashboardContainer } from "@/components/Dashboard/DashboardContainer"
 import { DashboardHeader } from "@/components/Dashboard/DashboardHeader"
-import Scoreboard from "@/components/Dashboard/Scoreboard"
 
-async function getScoreboard(user: User["id"]) {
-  const scoreboard = await prisma.quizCategory.findMany({
-    include: {
-      quizzes: {
-        where: {
-          quizScores: {
-            some: { userId: user },
-          },
-        },
+const page = () => {
+  const { data: session } = useSession()
 
-        include: {
-          quizScores: {
-            where: {
-              userId: user,
-            },
-            orderBy: { createdAt: "desc" },
-          },
-        },
-      },
-    },
-  })
-
-  return JSON.parse(JSON.stringify(scoreboard))
-}
-
-async function getAggregations(user: User["id"]) {
-  const aggregations = await prisma.quizScore.groupBy({
-    by: ["userId", "quizId"],
-    where: {
-      userId: user,
-    },
-    _count: {
-      score: true,
-    },
-    _avg: {
-      score: true,
-    },
-  })
-
-  return JSON.parse(JSON.stringify(aggregations))
-}
-
-async function getUser() {
-  const session = await fetch(
-    "https://quizander-dqzb.vercel.app/api/users/get-user"
-  )
-
-  return session.json()
-}
-
-export default async function ScoreboardPage() {
-  const session = await getUser()
-  console.log(session)
-
-  // if (!user) {
-  //   redirect("/")
-  // }
-
-  // const scoreboard = await getScoreboard(user.id)
-  // const aggregations = await getAggregations(user.id)
-
+  if (!session) {
+    // Handle unauthenticated state, e.g. render a SignIn component
+    return <div>elo</div>
+  }
   return (
     <DashboardContainer>
       <DashboardHeader
         heading="User Scoreboard"
         text="View your historical results."
       />
-      <div className="grid gap-10">
-        {/* <Scoreboard scoreboard={scoreboard} aggregations={aggregations} /> */}
-      </div>
+      <div className="grid gap-10">User is logged in</div>
     </DashboardContainer>
   )
 }
+
+export default page
---
+++ components/Dashboard/ScoreboardClient.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import React from "react"
+import { useSession } from "next-auth/react"
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+import { Separator } from "../ui/separator"
+
+const Scoreboard = () => {
+  return (
+    <>
+      <div
+        className="
+       rounded-md border border-dark-200 dark:border-dark-400"
+      >
+        Test
+      </div>
+    </>
+  )
+}
+
+export default Scoreboard
