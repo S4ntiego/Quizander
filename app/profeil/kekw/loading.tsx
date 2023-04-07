aa24875976285f90733eaeb7384783b51c226dc2
S4ntiego <adamksiazek123@gmail.com>
2023-04-07 20:48:07 +0200

    removed loading from kekw

--- app/profeil/kekw/loading.tsx
+++
@@ -1,7 +0,0 @@
-import { Card } from "@/components/Card"
-import { DashboardContainer } from "@/components/Dashboard/DashboardContainer"
-import { DashboardHeader } from "@/components/Dashboard/DashboardHeader"
-
-export default function DashboardSettingsLoading() {
-  return <div>siemasiema</div>
-}
