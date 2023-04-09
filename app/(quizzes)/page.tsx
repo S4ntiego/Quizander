4dddcaf8e8b1306d2636bb69fb061320a7909d7f
S4ntiego <adamksiazek123@gmail.com>
2023-04-09 20:15:43 +0200

    get quizzes

--- app/(quizzes)/page.tsx
+++ app/(quizzes)/page.tsx
@@ -2,7 +2,7 @@ import Landing from "@/components/Landing"
 import QuizList from "@/components/QuizList"
 
 async function getQuizzes() {
-  const res = await fetch("api/quiz/get-quizzes")
+  const res = await fetch("/api/quiz/get-quizzes")
   // The return value is *not* serialized
   // You can return Date, Map, Set, etc.
 
