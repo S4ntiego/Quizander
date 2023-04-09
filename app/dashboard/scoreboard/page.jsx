cb39f558cef2dd43e46e14ff360bba121aa6866b
S4ntiego <adamksiazek123@gmail.com>
2023-04-09 13:58:35 +0200

    test

--- app/dashboard/scoreboard/page.jsx
+++ app/dashboard/scoreboard/page.jsx
@@ -2,8 +2,6 @@ import { Suspense } from "react"
 import Link from "next/link"
 
 import { UserNameForm2 } from "@/components/Dashboard/UserNameForm2"
-import Repo from "@/components/Repo"
-import RepoDirs from "@/components/RepoDirs"
 
 const RepoPage = ({ params: { name } }) => {
   return (
@@ -13,12 +11,6 @@ const RepoPage = ({ params: { name } }) => {
       </Link>
       <Suspense fallback={<div>Loading search...</div>}>
         <UserNameForm2 />
-      </Suspense>
-      <Suspense fallback={<div>Loading repo...</div>}>
-        <Repo name={name} />
-      </Suspense>
-      <Suspense fallback={<div>Loading directories...</div>}>
-        <RepoDirs name={name} />
       </Suspense>
     </div>
   )
