715f526390c213ee4f655b61c68bf7d2c79bfbe9
S4ntiego <adamksiazek123@gmail.com>
2023-04-10 21:10:12 +0200

    test

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -1,5 +1,4 @@
 import { Suspense } from "react"
-import Link from "next/link"
 
 import { DashboardContainer } from "@/components/Dashboard/DashboardContainer"
 import { DashboardHeader } from "@/components/Dashboard/DashboardHeader"
@@ -7,23 +6,16 @@ import { UserNameForm2 } from "@/components/Dashboard/UserNameForm2"
 
 export default async function ProfilePage() {
   return (
-    <div className="card">
-      <Link href="/dashboard" className="btn btn-back">
-        Back To Repositories
-      </Link>
-      <Suspense fallback="loading profile">
-        <DashboardContainer>
-          <DashboardHeader
-            heading="User Profile"
-            text="Manage your user profile."
-          />
-          <div className="grid gap-10">
-            <Suspense fallback="loading user profile">
-              <UserNameForm2 />
-            </Suspense>
-          </div>
-        </DashboardContainer>
-      </Suspense>
-    </div>
+    <DashboardContainer>
+      <DashboardHeader
+        heading="User Profile"
+        text="Manage your user profile."
+      />
+      <div className="grid gap-10">
+        <Suspense fallback="loading user profile">
+          <UserNameForm2 />
+        </Suspense>
+      </div>
+    </DashboardContainer>
   )
 }
