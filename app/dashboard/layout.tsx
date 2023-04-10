0b275da95ab2c195d0a54108f97e0c35d5afefec
S4ntiego <adamksiazek123@gmail.com>
2023-04-10 20:33:04 +0200

    user dropdown test

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,14 +3,14 @@ import React from "react"
 import { dashboardConfig } from "@/config/dashboard"
 import { DashboardNav } from "@/components/DashboardNav"
 import { SiteHeader } from "@/components/SiteHeader"
+import UserDropdown from "@/components/UserDropdown"
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="mx-auto flex flex-col space-y-6">
-      <SiteHeader
-        mainConfig={dashboardConfig.mainNav}
-        mobileConfig={dashboardConfig.mobileNav}
-      />
+      <div className="h-20">
+        <UserDropdown></UserDropdown>
+      </div>
       <div className="container grid gap-12 md:grid-cols-[200px_1fr]">
         <aside className="hidden w-[200px] flex-col md:flex">
           <DashboardNav />
