c9391b29414f18a39b0b417c32f84d65cf9f72cb
S4ntiego <adamksiazek123@gmail.com>
2023-04-10 19:48:22 +0200

    loading test

--- app/dashh/loading.tsx
+++ app/dashh/loading.tsx
@@ -4,15 +4,6 @@ import { DashboardHeader } from "@/components/Dashboard/DashboardHeader"
 
 export default function DashboardSettingsLoading() {
   return (
-    <DashboardContainer>
-      <DashboardHeader
-        heading="Scoreboard"
-        text="View your historical results."
-      />
-      <div className="grid gap-10">
-        <Card.Skeleton />
-        <Card.Skeleton />
-      </div>
-    </DashboardContainer>
+    <DashboardContainer className="h-full w-full animate-pulse"></DashboardContainer>
   )
 }
