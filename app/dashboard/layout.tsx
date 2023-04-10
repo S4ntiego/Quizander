b0d020794d77ce9d40e02437e3d5360887f1af83
S4ntiego <adamksiazek123@gmail.com>
2023-04-10 20:59:06 +0200

    loading test

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import Link from "next/link"
 
 import { dashboardConfig } from "@/config/dashboard"
 import { DashboardNav } from "@/components/DashboardNav"
@@ -7,14 +8,9 @@ import { SiteHeader } from "@/components/SiteHeader"
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="mx-auto flex flex-col space-y-6">
-      <SiteHeader
-        mainConfig={dashboardConfig.mainNav}
-        mobileConfig={dashboardConfig.mobileNav}
-      />
+      <Link href="/dashboard/profile">Profile</Link>
+      <Link href="/dashboard/quizzes">Quizzes</Link>
       <div className="container grid gap-12 md:grid-cols-[200px_1fr]">
-        <aside className="hidden w-[200px] flex-col md:flex">
-          <DashboardNav />
-        </aside>
         <main className="flex w-full flex-1 flex-col overflow-hidden">
           {children}
         </main>
