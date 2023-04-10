73fb53075756eba248ea4d6485e730c9e70e5ac9
S4ntiego <adamksiazek123@gmail.com>
2023-04-10 16:29:05 +0200

    dashboard test

--- app/(dash)/dash/layout.tsx
+++ app/(dash)/dash/layout.tsx
@@ -12,21 +12,19 @@ export default async function DashboardLayout({
   children,
 }: DashboardLayoutProps) {
   return (
-    <Suspense fallback={<div>Loading quizzes...</div>}>
-      <div className="mx-auto flex flex-col space-y-6">
-        <SiteHeader
-          mainConfig={dashboardConfig.mainNav}
-          mobileConfig={dashboardConfig.mobileNav}
-        />
-        <div className="container grid gap-12 md:grid-cols-[200px_1fr]">
-          <aside className="hidden w-[200px] flex-col md:flex">
-            <DashboardNav />
-          </aside>
-          <main className="flex w-full flex-1 flex-col overflow-hidden">
-            {children}
-          </main>
-        </div>
+    <div className="mx-auto flex flex-col space-y-6">
+      <SiteHeader
+        mainConfig={dashboardConfig.mainNav}
+        mobileConfig={dashboardConfig.mobileNav}
+      />
+      <div className="container grid gap-12 md:grid-cols-[200px_1fr]">
+        <aside className="hidden w-[200px] flex-col md:flex">
+          <DashboardNav />
+        </aside>
+        <main className="flex w-full flex-1 flex-col overflow-hidden">
+          {children}
+        </main>
       </div>
-    </Suspense>
+    </div>
   )
 }
