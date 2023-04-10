15609e54af810b385c1b968b84e080418bf9dccd
S4ntiego <adamksiazek123@gmail.com>
2023-04-10 15:05:28 +0200

    dropdown test

--- app/(dash)/dash/layout.tsx
+++ app/(dash)/dash/layout.tsx
@@ -4,7 +4,7 @@ import { dashboardConfig } from "@/config/dashboard"
 import { getCurrentUser } from "@/lib/session"
 import { DashboardNav } from "@/components/DashboardNav"
 import { MainNav } from "@/components/MainNav"
-import UserDropdown from "@/components/UserDropdown"
+import { UserDropdown } from "@/components/UserDropdown"
 
 interface DashboardLayoutProps {
   children?: React.ReactNode
@@ -24,7 +24,13 @@ export default async function DashboardLayout({
       <header className="container sticky top-0 z-40 bg-white">
         <div className="flex h-16 items-center justify-between border-b border-b-slate-200 py-4">
           <MainNav mainConfig={dashboardConfig.mainNav} />
-          <UserDropdown />
+          <UserDropdown
+            user={{
+              name: user.name,
+              image: user.image,
+              email: user.email,
+            }}
+          />
         </div>
       </header>
       <div className="container grid gap-12 md:grid-cols-[200px_1fr]">
--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -1,120 +1,65 @@
 "use client"
 
-import React from "react"
 import Link from "next/link"
-import { LogOut, Settings, Trophy, User } from "lucide-react"
-import { signOut, useSession } from "next-auth/react"
+import { User } from "next-auth"
+import { signOut } from "next-auth/react"
 
-import { siteConfig } from "@/config/site"
-import { Icons } from "@/components/Icons"
-import { Avatar, AvatarImage } from "@/components/ui/avatar"
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuGroup,
   DropdownMenuItem,
-  DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
-import { ThemeToggle } from "./ThemeToggle"
-import { Button, buttonVariants } from "./ui/button"
-import { DropdownMenuShortcut } from "./ui/dropdown-menu"
+import { UserAvatar } from "./UserAvatar"
 
-const UserDropdown = () => {
-  const { data: session } = useSession()
-  const { email, image } = session?.user || {}
-
-  if (!email) return null
-
-  return (
-    <div className="relative inline-block text-left">
-      <div className="flex">
-        <nav className="mr-2 flex items-center space-x-1">
-          <Link href={siteConfig.links.github} target="_blank" rel="noreferrer">
-            <div
-              className={buttonVariants({
-                size: "sm",
-                variant: "ghost",
-                className: "text-slate-700 dark:text-slate-400",
-              })}
-            >
-              <Icons.gitHub className="h-5 w-5" />
-              <span className="sr-only">GitHub</span>
-            </div>
-          </Link>
-          <Link
-            href={siteConfig.links.linkedin}
-            target="_blank"
-            rel="noreferrer"
-          >
-            <div
-              className={buttonVariants({
-                size: "sm",
-                variant: "ghost",
-                className: "text-slate-700 dark:text-slate-400",
-              })}
-            >
-              <Icons.linkedIn className="h-5 w-5 fill-current" />
-              <span className="sr-only">LinkedIn</span>
-            </div>
-          </Link>
-          <ThemeToggle />
-        </nav>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button
-              variant="ghost"
-              className="relative flex items-center justify-center h-10 w-10 bg-center rounded-full"
-            >
-              <Avatar>
-                <AvatarImage src={`${image}`} alt={"A"} />
-              </Avatar>
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent
-            align="end"
-            sideOffset={24}
-            alignOffset={0}
-            className="w-[200px] slide-in-from-top-2 overflow-auto"
-            forceMount
-          >
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <Link href="/dash/profile">
-                <DropdownMenuItem>
-                  <User className="mr-2 h-4 w-4" />
-                  <span>Profile</span>
-                  <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
-                </DropdownMenuItem>
-              </Link>
-              <Link href="/dash/scoreboard">
-                <DropdownMenuItem>
-                  <Trophy className="mr-2 h-4 w-4" />
-                  <span>Scoreboard</span>
-                  <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
-                </DropdownMenuItem>
-              </Link>
-              <Link href="/dash/quizzes">
-                <DropdownMenuItem>
-                  <Settings className="mr-2 h-4 w-4" />
-                  <span>Quizzes</span>
-                  <DropdownMenuShortcut>⌘Q</DropdownMenuShortcut>
-                </DropdownMenuItem>
-              </Link>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem onClick={() => signOut()}>
-              <LogOut className="mr-2 h-4 w-4" />
-              <span>Log out</span>
-              <DropdownMenuShortcut>⇧⌘L</DropdownMenuShortcut>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </div>
-    </div>
-  )
+interface UserAccountNavProps extends React.HTMLAttributes<HTMLDivElement> {
+  user: Pick<User, "name" | "image" | "email">
 }
 
-export default UserDropdown
+export function UserDropdown({ user }: UserAccountNavProps) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <UserAvatar
+          user={{ name: user.name || null, image: user.image || null }}
+          className="h-8 w-8"
+        />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <div className="flex items-center justify-start gap-2 p-2">
+          <div className="flex flex-col space-y-1 leading-none">
+            {user.name && <p className="font-medium">{user.name}</p>}
+            {user.email && (
+              <p className="w-[200px] truncate text-sm text-slate-600">
+                {user.email}
+              </p>
+            )}
+          </div>
+        </div>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild>
+          <Link href="/dash">Dashboard</Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem asChild>
+          <Link href="/dash/profile">Profile</Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem asChild>
+          <Link href="/dash/scoreboard">Scoreboard</Link>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+          className="cursor-pointer"
+          onSelect={(event) => {
+            event.preventDefault()
+            signOut({
+              callbackUrl: `${window.location.origin}/login`,
+            })
+          }}
+        >
+          Sign out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
