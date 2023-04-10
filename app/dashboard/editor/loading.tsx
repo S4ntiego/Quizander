c3f5971a961ccc2c85aa10a56f5d4dad8efd7488
S4ntiego <adamksiazek123@gmail.com>
2023-04-10 11:47:13 +0200

    changed editor loading

--- app/dashboard/editor/loading.tsx
+++ app/dashboard/editor/loading.tsx
@@ -1,18 +1,10 @@
 import { DashboardContainer } from "@/components/Dashboard/DashboardContainer"
-import { DashboardHeader } from "@/components/Dashboard/DashboardHeader"
 import { Skeleton } from "@/components/Skeleton"
 
 export default function DashboardLoading() {
   return (
-    <DashboardContainer>
-      <div className="divide-y divide-dark-200 rounded-md border border-dark-200">
-        <div className="p-4">
-          <Skeleton className="h-7 w-2/5" />
-        </div>
-        <div className="p-4">
-          <Skeleton className="h-7 w-2/5" />
-        </div>
-      </div>
+    <DashboardContainer className="h-full w-full">
+      <Skeleton className="h-full w-full" />
     </DashboardContainer>
   )
 }
