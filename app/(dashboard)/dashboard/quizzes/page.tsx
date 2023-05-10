368a1331c1c6d89366dd698c07d906a46dd3c7aa
S4ntiego <adamksiazek123@gmail.com>
2023-05-10 11:20:30 +0200

    dashboard quizzes test

--- app/(dashboard)/dashboard/quizzes/page.tsx
+++ app/(dashboard)/dashboard/quizzes/page.tsx
@@ -12,30 +12,30 @@ import { EmptyPlaceholder } from "@/components/Dashboard/EmptyPlaceholder"
 import { Icons } from "@/components/Icons"
 import { buttonVariants } from "@/components/ui/button"
 
-const getQuizzes = cache(async () => {
-  const quizzes = await prisma.quiz.findMany({
-    include: {
-      questions: {
-        select: {
-          question: true,
-          answers: { select: { answer: true, isCorrect: true } },
-        },
-      },
-    },
-    orderBy: {
-      createdAt: "desc",
-    },
-  })
+// const getQuizzes = cache(async () => {
+//   const quizzes = await prisma.quiz.findMany({
+//     include: {
+//       questions: {
+//         select: {
+//           question: true,
+//           answers: { select: { answer: true, isCorrect: true } },
+//         },
+//       },
+//     },
+//     orderBy: {
+//       createdAt: "desc",
+//     },
+//   })
 
-  if (quizzes) {
-    for (const quiz of quizzes) {
-      quiz.coverImage =
-        "https://d16toh0t29dtt4.cloudfront.net/" + quiz.coverImage
-    }
-  }
+//   if (quizzes) {
+//     for (const quiz of quizzes) {
+//       quiz.coverImage =
+//         "https://d16toh0t29dtt4.cloudfront.net/" + quiz.coverImage
+//     }
+//   }
 
-  return quizzes
-})
+//   return quizzes
+// })
 
 export default async function DashboardPage() {
   const user = getCurrentUser()
@@ -44,7 +44,7 @@ export default async function DashboardPage() {
     redirect("/")
   }
 
-  const quizzes = await getQuizzes()
+  // const quizzes = await getQuizzes()
 
   return (
     <DashboardContainer>
@@ -61,7 +61,7 @@ export default async function DashboardPage() {
         </Link>
       </DashboardHeader>
 
-      <div>
+      {/* <div>
         {quizzes?.length ? (
           <div className="divide-y dark:divide-dark-400 divide-dark-200 rounded-md border dark:border-dark-400 border-dark-200">
             {quizzes.map((quiz) => (
@@ -86,7 +86,7 @@ export default async function DashboardPage() {
             </Link>
           </EmptyPlaceholder>
         )}
-      </div>
+      </div> */}
     </DashboardContainer>
   )
 }
